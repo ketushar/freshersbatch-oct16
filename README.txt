c65d0b776c2d1a62d967d8d3d15918c0c57327ac
ketushar <kanchan.d410@gmail.com>
2022-02-07 12:49:15 +0530

    commit for freshersbatch-oct16

---
+++ README.txt
@@ -0,0 +1,4 @@
+This is my js assignments
+This is my css Assignment
+This is my html Assignment for capgemini.
+ This is third line for css assignments.
---
+++ thirdDemo.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<title>W3.JS</title>
+<script src="https://www.w3schools.com/lib/w3.js"></script>
+
+<body>
+
+<button onclick="w3.hide('p')">Hide all p elements</button>
+
+<h2>DELHI</h2>
+<p>DELHI is the capital city of INDIA.</p>
+<p>It is the most populous city in the ASIA.</p>
+<p>Population over 13 million inhabitants.</p>
+
+</body>
+</html>
